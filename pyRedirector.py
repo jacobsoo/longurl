f38d375733687f1e282b4e4c65afe63fddbdd53d
Jacob Soo <jacobsoo@users.noreply.github.com>
2013-09-16 14:23:45 +0800

    Update pyRedirector.py

--- pyRedirector.py
+++ pyRedirector.py
@@ -1,12 +1,16 @@
 import urllib2, httplib
+redirectNode = []
 
 class RedirectHandler(urllib2.HTTPRedirectHandler):
+    
     def http_error_301(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_301(self, req, fp, code, msg, headers)
         result.status = code
+        redirectNode.append(headers['location'])
         return result
 
     def http_error_302(self, req, fp, code, msg, headers):
         result = urllib2.HTTPRedirectHandler.http_error_302(self, req, fp, code, msg, headers)
         result.status = code
+        redirectNode.append(headers['location'])
         return result
